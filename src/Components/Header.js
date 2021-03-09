9d5b7a97383db43ab0dad0fd86b89b83e7f8e049
Davy <davyfungaloi@gmail.com>
2021-03-09 16:59:28 +0100

    added images for the navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import GitHubIcon from "../Images/GitHubIcon.png";
+import LinkedinIcon from "../Images/LinkedinIcon.png";
 
 const Header = () => {
   return (
@@ -9,9 +11,17 @@ const Header = () => {
       <a href="portfolio">Projects</a>
       <a href="blog">Blog</a>
       <a href="about">About me</a>
-      <a class = "Header__Contact"href="contact">Contact</a>
-      <a href="https://github.com/DavyFungALoi">Linkedin</a>
-      <a href="https://www.linkedin.com/in/davyfungaloi/">Github</a>
+      <a class="Header__Contact" href="contact">
+        Contact
+      </a>
+      <div class="Header__IconContainer">
+        <a href="https://www.linkedin.com/in/davyfungaloi/">
+          <img src={LinkedinIcon} alt="LinkedinIcon"></img>
+        </a>
+        <a href="https://github.com/DavyFungALoi">
+          <img src={GitHubIcon} alt="GithubIcon"></img>
+        </a>
+      </div>
     </div>
   );
 };
