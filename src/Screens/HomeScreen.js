87858ab147944c95ab3a72c9ddfefd201a174635
Davy <davyfungaloi@gmail.com>
2021-03-11 21:44:23 +0100

    added the base CTA buttons and intro

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,33 @@
 import React from "react";
-import Github from "../Images/GitHub-Mark.png";
-import Linkedin from "../Images/Linkedin.svg";
+import GitHubIcon from "../Images/GitHubIcon.png";
+import LinkedinIcon from "../Images/LinkedinIcon.png";
+import MouseWheelIcon from "../Images/MouseWheelIcon"
 
 const HomeScreen = () => {
   return (
-    <div className="HomeScreenContainer">
-  <div>Hello</div>
-    </div>
+    <>
+      <div className="Introduction__Container">
+        <h1>Davy Fung-A-Loi</h1>
+        <h2>Fullstack Developer</h2>
+        <div className="Introduction__Container__Intro">
+          Hi! I’m a fullstack developer doing cool things. Small intro about me
+          that briefly summarises what it is that I do and just generally adds a
+          bit more content here.{" "}
+        </div>
+        <div className="Introduction__Container__Button__Container">
+          <button>My Projects ></button>
+          <button>Contact me ></button>
+        </div>
+      </div>
+      <div className="Social__Links__Container">
+        <a href="https://www.linkedin.com/in/davyfungaloi/">
+          <img src={LinkedinIcon} alt="LinkedinIcon"></img>
+        </a>
+        <a href="https://github.com/DavyFungALoi">
+          <img src={GitHubIcon} alt="GithubIcon"></img>
+        </a>
+      </div>
+    </>
   );
 };
 
