a0848226f5fdcc3d7a62c87f4b3aa841cf1d33ad
Davy <davyfungaloi@gmail.com>
2021-03-11 22:23:56 +0100

    finished above the fold work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from "./logo.svg";
-import "../src/css/App.css"
-///import "./App.css"
+import "../src/css/App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./Components/Header";
-import Footer from "./Components/Footer";
 import HomeScreen from "./Screens/HomeScreen";
 import BlogListScreen from "./Screens/BlogListScreen";
 import { ContactScreen } from "./Screens/ContactScreen";
@@ -13,18 +10,14 @@ import PortFolioListScreen from "./Screens/PortFolioListScreen";
 function App() {
   return (
     <Router>
- <div className="WebsiteWideContainer">
-       
-          <Header></Header>
-        
+      <div className="WebsiteWideContainer">
+        <Header></Header>
+
         <div className="MainContainer">
           <Route path="/" component={HomeScreen} exact />
           <Route path="/blog" component={BlogListScreen} exact />
           <Route path="/contact" component={ContactScreen} exact />
           <Route path="/projects" component={PortFolioListScreen} exact />
-        </div>
-        <div className="FooterContainer">
-          <Footer></Footer>
         </div>
       </div>
     </Router>
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import GitHubIcon from "../Images/GitHubIcon.png";
 import LinkedinIcon from "../Images/LinkedinIcon.png";
-import MouseWheelIcon from "../Images/MouseWheelIcon"
+import MouseWheelIconDownwards from "../Images/MouseWheelIconDownwards.png";
+import { BiMouse } from "react-icons/bi";
 
 const HomeScreen = () => {
   return (
@@ -12,11 +13,11 @@ const HomeScreen = () => {
         <div className="Introduction__Container__Intro">
           Hi! I’m a fullstack developer doing cool things. Small intro about me
           that briefly summarises what it is that I do and just generally adds a
-          bit more content here.{" "}
+          bit more content here.
         </div>
         <div className="Introduction__Container__Button__Container">
-          <button>My Projects ></button>
-          <button>Contact me ></button>
+        <a href="portfolio"><button>My Projects ></button></a>
+        <a href="contact"><button>Contact Me ></button></a>
         </div>
       </div>
       <div className="Social__Links__Container">
@@ -26,6 +27,10 @@ const HomeScreen = () => {
         <a href="https://github.com/DavyFungALoi">
           <img src={GitHubIcon} alt="GithubIcon"></img>
         </a>
+      </div>
+      <div className="Scroll__Downwards__Container">
+        <img src={MouseWheelIconDownwards} alt="MouseWheelIconDownwards"></img>
+        <div>Scroll for more about me</div>
       </div>
     </>
   );
