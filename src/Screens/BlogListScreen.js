fa4bff3560cc16138017d22435f308b71139a07b
Davy <davyfungaloi@gmail.com>
2021-04-01 22:07:45 +0200

    fixed some skeleton for the blog section

--- src/Screens/BlogListScreen.js
+++ src/Screens/BlogListScreen.js
@@ -3,9 +3,11 @@ import RandomFunFact from "../Components/RandomFunFact";
 
 const BlogListScreen = () => {
   return (
-    <div>
-      <RandomFunFact></RandomFunFact>
-   
+    <div className="BlogContainer">
+      <h2>Blog Coming Soon</h2>
+      <a href="/">
+        <button className="Blog_Screen_Button">Go back to Home for now</button>
+      </a>
     </div>
   );
 };
